03f9b440916716f56b728803ce41721227c1e28c
Cahp2025dev <scorpio.007.lp@gmail.com>
2025-08-29 10:40:12 -0500

    v4

--- index.sh
+++ index.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 echo "hola, desde la rama2"
-=======
-echo "Gracias Rama2 y Rama3. Son muy lindos ustedes :) Atte: Master!"
->>>>>>> master
--- saludo.py
+++ saludo.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 print("Mensaje de hoy: Sin cambios")
-=======
-print("Mensaje de hoy: Agradezco a Rama2 y Rama3 por su apoyo")
->>>>>>> master
+
