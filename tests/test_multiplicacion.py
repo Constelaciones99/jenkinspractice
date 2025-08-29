1f822f453d474aa02a1b6e1a0fbe9c8ff704551c
Cahp2025dev <scorpio.007.lp@gmail.com>
2025-08-29 12:06:52 -0500

    jenkins11

--- tests/test_multiplicacion.py
+++ tests/test_multiplicacion.py
@@ -9,6 +9,8 @@ def test_multiplicar():
     assert multiplicar(-1, 5) == -5
     assert multiplicar(0, 100) == 0
     assert multiplicar(-2, -4) == 8
-    
+
+@pytest.mark.xfail(reason="Este test está diseñado para fallar")
 def test_multiplicar_fallo():
+    pytest.xfail("Este test está diseñado para fallar")
     assert multiplicar(2, 3) == 5  # Este test está diseñado para fallar
