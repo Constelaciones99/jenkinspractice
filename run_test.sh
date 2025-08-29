49e6d053574433d7f26f2660c10522c6218f81de
Cahp2025dev <scorpio.007.lp@gmail.com>
2025-08-29 11:59:37 -0500

    opc2

--- run_test.sh
+++ run_test.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
+# Crear directorio de reports si no existe
+mkdir -p reports
+
 echo "Activando entorno virtual..."
 if [ ! -d "venv" ]; then
-echo "El entorno virtual no existe. Creando uno nuevo..."
-    python -m venv venv
+    echo "El entorno virtual no existe. Creando uno nuevo..."
+    python3 -m venv venv
 fi
 
+# Determinar la ruta correcta según el sistema
 if [ -f "venv/bin/activate" ]; then
     source venv/bin/activate
+    PYTHON_EXEC="venv/bin/python"
 elif [ -f "venv/Scripts/activate" ]; then
     source venv/Scripts/activate
+    PYTHON_EXEC="venv/Scripts/python"
 else
     echo "No se pudo encontrar el script de activación del entorno virtual."
     exit 1
 fi
 
-source venv/Scripts/activate
-
 echo "Instalando dependencias..."
-pip install --upgrade pip --break-system-packages
-pip install -r requirements.txt --break-system-packages
+pip install --upgrade pip
+pip install -r requirements.txt
 
-echo "Ejecutando pruebas con pytest ..."
-venv/bin/python  pytest tests/ --junit-xml=reports/test-results.xml --html=reports/test-report.html --self-contained-html
+echo "Ejecutando pruebas con pytest..."
+$PYTHON_EXEC -m pytest tests/ --junit-xml=reports/test-results.xml --html=reports/test-report.html --self-contained-html
 
 echo "Pruebas completadas. Los informes se encuentran en el directorio 'reports'."
