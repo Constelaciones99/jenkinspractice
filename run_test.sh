cdf4d051b4280dbb7f139e1695a9069350a51db6
Cahp2025dev <scorpio.007.lp@gmail.com>
2025-08-29 11:05:39 -0500

    jenkins14

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Activando entorno virtual..."
-if[ ! -d "venv" ]; then
+if [ ! -d "venv" ]; then
 echo "El entorno virtual no existe. Creando uno nuevo..."
     python -m venv venv
 fi
@@ -18,10 +18,10 @@ fi
 source venv/Scripts/activate
 
 echo "Instalando dependencias..."
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 echo "Ejecutando pruebas con pytest ..."
-pytest tests/ --junit-xml=reports/test-results.xml --html=reports/test-report.html --self-contained-html
+venv/bin/python -m  pytest tests/ --junit-xml=reports/test-results.xml --html=reports/test-report.html --self-contained-html
 
 echo "Pruebas completadas. Los informes se encuentran en el directorio 'reports'."
